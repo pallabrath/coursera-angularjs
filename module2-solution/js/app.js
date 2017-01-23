971f7d917669741ad448ddab76d3ad67683d7ec4
pallabrath <pallab_rath@hotmail.com>
2017-01-23 19:09:25 +0530

    changed to complete assignment.

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -3,15 +3,34 @@
 'use strict;'
  angular.module('ShoppingCart',[])
   .controller('ToBuyController',ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
   .service('ShoppingCartService',ShoppingCartService);
   ToBuyController.$inject = ['$scope','ShoppingCartService'];
+  AlreadyBoughtController.$inject = ['$scope','ShoppingCartService'];
   function ToBuyController($scope, ShoppingCartService)
   {
     $scope.catlog = ShoppingCartService.catlog;
+    $scope.addToCart = function(index) {
+      ShoppingCartService.addToCart(index);
+    }
+  }
+  function AlreadyBoughtController($scope, ShoppingCartService)
+  {
+    $scope.alreadyBoughtItems = ShoppingCartService.alreadyBoughtItems;
   }
   function ShoppingCartService()
   {
-    this.catlog = "I am testing";
+    var ShoppingCartService = this;
+    ShoppingCartService.catlog = [{name: 'chips', quantity : 2 },{name: 'candy', quantity : 3},
+  {name: 'bread', quantity : 2 },{name: 'jam', quantity : 1 },
+  {name: 'butter', quantity : 1 },{name: 'milk', quantity : 4 }];
+    ShoppingCartService.alreadyBoughtItems = [];
+    ShoppingCartService.addToCart = function(index)
+    {
+      //console.log("I am right here "+index);
+      ShoppingCartService.alreadyBoughtItems.push(ShoppingCartService.catlog[index]);
+      ShoppingCartService.catlog.splice(index, 1);
+    }
   }
 }
 )();
